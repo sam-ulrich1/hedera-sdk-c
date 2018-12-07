dc8be4f1a8c2db615d4c0b72405eb81fc4f258f0
Ryan Leckey <ryan@launchbadge.com>
2018-12-06 18:33:31 -0800

    Use a better name for HederaTransactionReceipt

--- include/hedera-query-get-transaction-receipt.h
+++ include/hedera-query-get-transaction-receipt.h
@@ -9,18 +9,19 @@ extern "C" {
 #endif
 
 typedef struct {
+    // fixme: use enum here
     uint8_t status;
     HederaAccountId* account_id;
     HederaContractId* contract_id;
     HederaFileId* file_id;
-} HederaQueryGetTransactionReceiptAnswer;
+} HederaTransactionReceipt;
 
 extern HederaQuery* hedera_query__get_transaction_receipt__new(
     HederaClient*,
     HederaTransactionId transaction_id
 );
 
-extern HederaError hedera_query__get_transaction_receipt__get(HederaQuery*, HederaQueryGetTransactionReceiptAnswer*);
+extern HederaError hedera_query__get_transaction_receipt__get(HederaQuery*, HederaTransactionReceipt*);
 
 #ifdef __cplusplus
 }
