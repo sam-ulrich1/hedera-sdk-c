506441326c4238c3c9a8b215a7216a9dd050fe6f
Ryan Leckey <ryan@launchbadge.com>
2018-12-06 17:54:14 -0800

    Actually rename the rust side

--- src/query_crypto_get_account_balance.rs
+++ src/query_crypto_get_account_balance.rs
@@ -1,7 +1,7 @@
 use hedera::{query::QueryCryptoGetAccountBalance, AccountId};
 
 def_query_new!(
-    QueryCryptoGetAccountBalance: hedera_query__get_account_balance__new(AccountId) -> u64
+    QueryCryptoGetAccountBalance: hedera_query__crypto_get_account_balance__new(AccountId) -> u64
 );
 
-def_query_get!(hedera_query__get_account_balance__get -> u64);
+def_query_get!(hedera_query__crypto_get_account_balance__get -> u64);
