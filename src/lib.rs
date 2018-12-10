cfec31f5bf874b3a298d766769df2fe223a5caaf
Josh <josh@launchbadge.com>
2018-12-10 13:42:38 -0800

    transaction contract create

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,8 @@ mod query_get_transaction_receipt;
 
 mod transaction;
 
+mod transaction_contract_create;
+
 mod transaction_crypto_create;
 mod transaction_crypto_delete;
 mod transaction_crypto_transfer;
---
+++ src/transaction_contract_create.rs
@@ -0,0 +1,36 @@
+use hedera::PublicKey;
+use hedera::transaction::TransactionContractCreate;
+use hedera::AccountId;
+use hedera::FileId;
+
+def_tx_new!(TransactionContractCreate: hedera_transaction__contract_create__new);
+
+def_tx_method!(TransactionContractCreate: hedera_transaction__contract_create__set_file(FileId): file);
+
+def_tx_method!(
+    TransactionContractCreate: hedera_transaction__contract_create__set_gas(i64): gas
+);
+
+def_tx_method!(
+    TransactionContractCreate: hedera_transaction__contract_create__set_admin_key(PublicKey): admin_key
+);
+
+def_tx_method!(
+    TransactionContractCreate: hedera_transaction__contract_create__set_initial_balance(i64): initial_balance
+);
+
+def_tx_method!(
+    TransactionContractCreate: hedera_transaction__contract_create__set_proxy_account(AccountId): proxy_account
+);
+
+def_tx_method!(
+    TransactionContractCreate: hedera_transaction__contract_create__set_proxy_fraction(i32): proxy_fraction
+);
+
+//todo: auto_renew_period
+
+def_tx_method!(
+    TransactionContractCreate: hedera_transaction__contract_create__set_constructor_parameters(Vec<u8>): constructor_parameters
+);
+
+// todo: to proto
