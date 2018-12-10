69aa37f9e761d16ba7405e99ded423478d200585
Josh <josh@launchbadge.com>
2018-12-10 12:43:22 -0800

    trivial transaction_crypto_create function

--- include/hedera-transaction-crypto-create.h
+++ include/hedera-transaction-crypto-create.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "hedera-transaction.h"
+#include "hedera-id.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -11,6 +12,18 @@ extern HederaTransaction* hedera_transaction__crypto_create__new(HederaClient*);
 extern void hedera_transaction__crypto_create__set_initial_balance(HederaTransaction*, uint64_t balance);
 
 extern void hedera_transaction__crypto_create__set_key(HederaTransaction*, HederaPublicKey key);
+
+extern void hedera_transaction__crypto_create__set_proxy_account(HederaTransaction*, HederaAccountId id);
+
+extern void hedera_transaction__crypto_create__set_proxy_fraction(HederaTransaction*, int32_t fraction);
+
+extern void hedera_transaction__crypto_create__set_max_receive_proxy_faction(HederaTransaction*, int32_t fraction);
+
+extern void hedera_transaction__crypto_create__send_record_threshold(HederaTransaction*, int64_t threshold);
+
+extern void hedera_transaction__crypto_create__set_receive_record_threshold(HederaTransaction*, int64_t threshold);
+
+extern void hedera_transaction__crypto_create__set_receiver_signature_required(HederaTransaction*, bool required);
 
 #ifdef __cplusplus
 }
--- src/transaction_crypto_create.rs
+++ src/transaction_crypto_create.rs
@@ -1,5 +1,6 @@
 use hedera::PublicKey;
 use hedera::transaction::TransactionCryptoCreate;
+use hedera::AccountId;
 
 def_tx_new!(TransactionCryptoCreate: hedera_transaction__crypto_create__new);
 
@@ -10,4 +11,30 @@ def_tx_method!(
         initial_balance
 );
 
-// todo: the rest
+def_tx_method!(
+    TransactionCryptoCreate: hedera_transaction__crypto_create__set_proxy_account(AccountId): proxy_account
+);
+
+def_tx_method!(
+    TransactionCryptoCreate: hedera_transaction__crypto_create__set_proxy_fraction(i32): proxy_fraction
+);
+
+def_tx_method!(
+    TransactionCryptoCreate: hedera_transaction__crypto_create__set_max_receive_proxy_fraction(i32): max_receive_proxy_fraction
+);
+
+//todo: auto_renew_period
+
+def_tx_method!(
+    TransactionCryptoCreate: hedera_transaction__crypto_create__set_send_record_threshold(i64): send_record_threshold
+);
+
+def_tx_method!(
+    TransactionCryptoCreate: hedera_transaction__crypto_create__set_receive_record_threshold(i64): receive_record_threshold
+);
+
+def_tx_method!(
+    TransactionCryptoCreate: hedera_transaction__crypto_create__set_receiver_signature_required(bool): receiver_signature_required
+);
+
+// todo: to proto
