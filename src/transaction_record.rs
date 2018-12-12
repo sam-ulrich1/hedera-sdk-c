62a40e9f173fd462e9f58a45a8639d301681092c
Josh <josh@launchbadge.com>
2018-12-12 11:44:29 -0800

    fixed so it compiles

--- src/transaction_record.rs
+++ src/transaction_record.rs
@@ -2,14 +2,15 @@ use hedera::{Claim, AccountId, PublicKey };
 use std::convert::{TryInto, TryFrom};
 use failure::Error;
 use hedera::TransactionReceipt;
+use crate::timestamp::CTimestamp;
 
 #[repr(C)]
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone)]
 pub struct CTransactionRecord {
     pub(crate) receipt: TransactionReceipt,
     pub ( crate ) transaction_hash: * const u8, // pointer to hash
     pub ( crate ) transaction_hash_length: usize, // hash len
-    pub ( crate ) consensus_timestamp: Option<CTimeStamp>,
+    pub ( crate ) consensus_timestamp: Option<CTimestamp>,
     pub ( crate ) memo: String,
     pub ( crate ) transaction_fee: u64,
     // record body?
