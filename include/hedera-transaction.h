d5ee93f3ded0acbfac1781f29de0310d6e16e583
Greg Scullard <greg@scullard.com>
2019-02-15 15:21:34 +0100

    Update hedera-transaction.h

--- include/hedera-transaction.h
+++ include/hedera-transaction.h
@@ -20,7 +20,7 @@ extern void hedera_transaction_set_memo(HederaTransaction*, const char* memo);
 
 extern void hedera_transaction_sign(HederaTransaction*, HederaSecretKey*);
 
-extern void hedera_transaction_set_generate_record(HederaTransaction*, const u_int8_t record);
+extern void hedera_transaction_set_generate_record(HederaTransaction*, u_int8_t record);
 
 extern HederaError hedera_transaction_execute(HederaTransaction*, HederaTransactionId*);
 
